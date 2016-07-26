0c32a979042a11135bb4fe8e35fb090f4aa177f4
William Antero <wantero@dlctec.com.br>
2016-07-26 15:17:40 +0000

    Changes made by William Antero (wantero@dlctec.com.br).

--- Mackenzie/components/home/index.js
+++ Mackenzie/components/home/index.js
@@ -9,3 +9,138 @@ app.home = kendo.observable({
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_home
+(function(parent) {
+    var provider = app.data.mackenzie,
+        mode = 'signin',
+        registerRedirect = 'home',
+        signinRedirect = 'cursosView',
+        rememberKey = 'mackenzie_authData_homeModel',
+        init = function(error) {
+            if (error) {
+                if (error.message) {
+                    alert(error.message);
+                }
+                return false;
+            }
+
+            var activeView = mode === 'signin' ? '.signin-view' : '.signup-view',
+                model = parent.homeModel;
+
+            if (provider.setup && provider.setup.offlineStorage && !app.isOnline()) {
+                $('.offline').show().siblings().hide();
+            } else {
+                $(activeView).show().siblings().hide();
+            }
+
+            if (model && model.set) {
+                model.set('logout', null);
+            }
+
+            var rememberedData = localStorage ? JSON.parse(localStorage.getItem(rememberKey)) : app[rememberKey];
+            if (rememberedData && rememberedData.email && rememberedData.password) {
+
+                parent.homeModel.set('email', rememberedData.email);
+                parent.homeModel.set('password', rememberedData.password);
+                parent.homeModel.signin();
+            }
+        },
+        successHandler = function(data) {
+            var redirect = mode === 'signin' ? signinRedirect : registerRedirect,
+                model = parent.homeModel || {},
+                logout = model.logout;
+
+            if (logout) {
+                model.set('logout', null);
+            }
+            if (data && data.result) {
+                if (logout) {
+                    provider.Users.logout(init, init);
+                    return;
+                }
+                var rememberedData = {
+                    email: model.email,
+                    password: model.password
+                };
+                if (model.rememberme && rememberedData.email && rememberedData.password) {
+                    if (localStorage) {
+                        localStorage.setItem(rememberKey, JSON.stringify(rememberedData));
+                    } else {
+                        app[rememberKey] = rememberedData;
+                    }
+                }
+                app.user = data.result;
+
+                setTimeout(function() {
+                    app.mobileApp.navigate('components/' + redirect + '/view.html');
+                }, 0);
+            } else {
+                init();
+            }
+        },
+        homeModel = kendo.observable({
+            displayName: '',
+            email: '',
+            password: '',
+            validateData: function(data) {
+                if (!data.email) {
+                    alert('Missing email');
+                    return false;
+                }
+
+                if (!data.password) {
+                    alert('Missing password');
+                    return false;
+                }
+
+                return true;
+            },
+            signin: function() {
+                var model = homeModel,
+                    email = model.email.toLowerCase(),
+                    password = model.password;
+
+                if (!model.validateData(model)) {
+                    return false;
+                }
+                provider.Users.login(email, password, successHandler, init);
+            },
+            register: function() {
+                var model = homeModel,
+                    email = model.email.toLowerCase(),
+                    password = model.password,
+                    displayName = model.displayName,
+                    attrs = {
+                        Email: email,
+                        DisplayName: displayName
+                    };
+
+                if (!model.validateData(model)) {
+                    return false;
+                }
+
+                provider.Users.register(email, password, attrs, successHandler, init);
+            },
+            toggleView: function() {
+                mode = mode === 'signin' ? 'register' : 'signin';
+                init();
+            }
+        });
+
+    parent.set('homeModel', homeModel);
+    parent.set('afterShow', function(e) {
+        if (e && e.view && e.view.params && e.view.params.logout) {
+            if (localStorage) {
+                localStorage.setItem(rememberKey, null);
+            } else {
+                app[rememberKey] = null;
+            }
+            homeModel.set('logout', true);
+        }
+        provider.Users.currentUser().then(successHandler, init);
+    });
+})(app.home);
+
+// START_CUSTOM_CODE_homeModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_homeModel
