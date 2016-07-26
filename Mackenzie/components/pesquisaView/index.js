bf0d4a1abd8a152ba790953782b278558d91dbce
William Antero <wantero@dlctec.com.br>
2016-07-26 17:46:05 +0000

    Changes made by William Antero (wantero@dlctec.com.br).

---
+++ Mackenzie/components/pesquisaView/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.pesquisaView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_pesquisaView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_pesquisaView
