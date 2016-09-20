12beeaf4478d7727f7c7ae91e26b322ca9222e4a
rogerio.rueda <rogerio.rueda@gmail.com>
2016-09-20 16:02:05 -0300

    Teste FilePicker (correcao)

--- Mackenzie/components/perfilView/index.js
+++ Mackenzie/components/perfilView/index.js
@@ -79,13 +79,12 @@ app.perfilView = kendo.observable({
 
             if (window.navigator.simulator === true) {
               alert('This plugin is not available in the simulator.');
-                return true;
-              } else if (window.FilePicker === undefined) {
-                alert('Plugin not found. Maybe you are running in AppBuilder Companion app which currently does not support this plugin.');
-                return true;
-              } else {
-                return false;
-              }
+              return true;
+            } else if (window.FilePicker === undefined) {
+              alert('Plugin not found. Maybe you are running in AppBuilder Companion app which currently does not support this plugin.');
+              return true;
+            } else {
+              return false;
             }
 
             FilePicker.isAvailable(function (avail) {
